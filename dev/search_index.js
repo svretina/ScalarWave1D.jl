a5d03a6cfad844788240b7ae9fc8b7cbca0e8360
Documenter.jl <documenter@juliadocs.github.io>
2023-08-23 14:32:28 +0000

    build based on fab0421

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ScalarWave1D","category":"page"},{"location":"#ScalarWave1D","page":"Home","title":"ScalarWave1D","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ScalarWave1D.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ScalarWave1D]","category":"page"}]
+}
